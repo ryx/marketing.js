ffbf8b135296d7c6bae29d3b9c73ad78758df81b
Rico Pfaus <rico.pfaus@kaufhof.de>
2018-11-16 17:09:45 +0100

    fixed webpack entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/marketing.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'marketing.umd.js',
